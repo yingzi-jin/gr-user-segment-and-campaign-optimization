d1e67354d911f8617b13e9be32aa384af2ca8706
yingzi-jin <yzjin2006@gmail.com>
2013-10-09 15:48:20 +0900

    Create run_set_sleeping_prevention_mail.bat

---
+++ run_set_sleeping_prevention_mail.bat
@@ -0,0 +1,26 @@
+c:
+cd %~dp0
+python set_sleeping_prevention_mail.py 2013 01 01
+python set_sleeping_prevention_mail.py 2013 01 02
+python set_sleeping_prevention_mail.py 2013 01 03
+python set_sleeping_prevention_mail.py 2013 01 04
+python set_sleeping_prevention_mail.py 2013 01 05
+python set_sleeping_prevention_mail.py 2013 01 06
+python set_sleeping_prevention_mail.py 2013 01 07
+python set_sleeping_prevention_mail.py 2013 01 08
+python set_sleeping_prevention_mail.py 2013 01 09
+python set_sleeping_prevention_mail.py 2013 01 10
+python set_sleeping_prevention_mail.py 2013 01 11
+python set_sleeping_prevention_mail.py 2013 01 12
+python set_sleeping_prevention_mail.py 2013 01 13
+python set_sleeping_prevention_mail.py 2013 01 14
+python set_sleeping_prevention_mail.py 2013 01 15
+python set_sleeping_prevention_mail.py 2013 01 16
+python set_sleeping_prevention_mail.py 2013 01 17
+python set_sleeping_prevention_mail.py 2013 01 18
+python set_sleeping_prevention_mail.py 2013 01 19
+python set_sleeping_prevention_mail.py 2013 01 20
+python set_sleeping_prevention_mail.py 2013 01 21
+python set_sleeping_prevention_mail.py 2013 01 22
+python set_sleeping_prevention_mail.py 2013 01 23
+pause
